f8c5cbb2948ab29a0457279454d83a48e7da9403
Daniil_Shakhmovedev <dshahmovedeev@intetech.ru>
2026-09-10 20:13:32 +0300

    init

---
+++ internal/calculator/lib_loader.go
@@ -0,0 +1,11 @@
+package calculator
+
+import "PythonToGoMigrationTest/internal/config"
+
+type LibLoader struct {
+	cfg *config.CalculatorConfig
+}
+
+func (ll LibLoader) load() {
+
+}
---
+++ internal/config/calculator_config.go
@@ -0,0 +1,9 @@
+package config
+
+type CalculatorConfig struct {
+	Host        string
+	Port        int
+	CLibPath    string
+	RustLibPath string
+	Interval    int
+}

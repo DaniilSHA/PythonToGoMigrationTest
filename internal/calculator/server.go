c2c798044662cdafbe97fecfebd1103f46ba34b6
Daniil_Shakhmovedev <dshahmovedeev@intetech.ru>
2026-09-10 22:49:44 +0300

    wip

---
+++ internal/calculator/server.go
@@ -0,0 +1,76 @@
+package calculator
+
+import (
+	"PythonToGoMigrationTest/internal/config"
+	"context"
+	"errors"
+	"log/slog"
+	"net"
+	"net/http"
+	"os"
+	"os/signal"
+	"strconv"
+	"syscall"
+	"time"
+)
+
+func NewHTTPServer(cfg *config.CalculatorConfig, state *State) *http.Server {
+	mux := http.NewServeMux()
+	mux.Handle("POST /calc", NewCalcHandler(state))
+	mux.HandleFunc("GET /health", func(w http.ResponseWriter, r *http.Request) {
+		_, _ = w.Write([]byte("ok"))
+	})
+
+	return &http.Server{
+		Addr:    net.JoinHostPort(cfg.Host, strconv.Itoa(cfg.Port)),
+		Handler: mux,
+	}
+}
+
+func runServer(server *http.Server, state *State, interval time.Duration) error {
+	ctx, stop := signal.NotifyContext(context.Background(), os.Interrupt, syscall.SIGTERM)
+	defer stop()
+
+	listener, err := net.Listen("tcp", server.Addr)
+	if err != nil {
+		return err
+	}
+
+	printerJobComplete := make(chan struct{})
+	go func() {
+		defer close(printerJobComplete)
+		periodicPrinterJob(ctx, state, interval)
+	}()
+
+	serverErrors := make(chan error, 1)
+	go func() {
+		serverErrors <- server.Serve(listener)
+	}()
+	slog.Info("calculator server listening", "address", listener.Addr().String())
+
+	var serveErr error
+	select {
+	case <-ctx.Done():
+		slog.Info("shutting down calculator server")
+	case serveErr = <-serverErrors:
+		if errors.Is(serveErr, http.ErrServerClosed) {
+			serveErr = nil
+		}
+	}
+	stop()
+
+	shutdownErr := shutdownServer(server)
+	<-printerJobComplete
+	state.PrintTotals("final")
+	return errors.Join(serveErr, shutdownErr)
+}
+
+func shutdownServer(server *http.Server) error {
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	if err := server.Shutdown(ctx); err != nil {
+		return errors.Join(err, server.Close())
+	}
+	return nil
+}
